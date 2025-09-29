c2df46f92e0fd89970f6af97c16d30b054aef307
PatriksM <pmorevs@gmail.com>
2025-09-29 20:22:09 +0300

    izveidojām Game Klase

--- cuska.py
+++ cuska.py
@@ -1,8 +1,12 @@
 from tkinter import Canvas, Tk #GUI bibliotēka TK Inter
 
+class Game:
+    def __init__(self, canvas):
+        pass
+
 root = Tk() #izveidojām objektu, kas ir lodziņš
 root.title("* * * Č Ū S K A * * *")
-canvas = Canvas(root, width=300, height=300, bg="white")
-canvas.pack()
-
+canvas = Canvas(root, width=300, height=300, bg="black")
+canvas.pack() #izvēlāmies ģeometrijas pārvaldienku, var ņemt: pack, place, grid
+game = Game(canvas)
 root.mainloop() #lai lodziņš nepazustu, lai visu laiku ir vaļā
