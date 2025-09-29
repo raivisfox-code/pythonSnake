46c58d6006ce2172c7a0cbd9aae5dd9e78b3f1d3
PatriksM <pmorevs@gmail.com>
2025-09-29 20:14:40 +0300

    Geometry manager added

--- cuska.py
+++ cuska.py
@@ -1,4 +1,8 @@
-from tkinter import Tk #GUI bibliotēka TK Inter
+from tkinter import Canvas, Tk #GUI bibliotēka TK Inter
 
 root = Tk() #izveidojām objektu, kas ir lodziņš
+root.title("* * * Č Ū S K A * * *")
+canvas = Canvas(root, width=300, height=300, bg="white")
+canvas.pack()
+
 root.mainloop() #lai lodziņš nepazustu, lai visu laiku ir vaļā
